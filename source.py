0fb3282ac82666256cdbab3f04748a862ff8fce6
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-09-10 00:44:24 -0300

    Update logger object to send output into a file

--- source.py
+++ source.py
@@ -2,26 +2,29 @@ import logging
 
 def compareStrings(a, b):
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-    logging.getLogger().setLevel(logging.INFO)
-    logging.info('Llamada a la función compareStrings!')
-    logging.info('Parametros: a => "{}" [type: {}] b=> "{}" [type: {}]'.format(a, type(a), b, type(b)))
+    log = logging.getLogger('compareStrings')
+    fh = logging.FileHandler('compareStrings.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S'))
+    log.addHandler(fh)
+    log.setLevel(logging.INFO)
+    log.info('Llamada a la función compareStrings!')
+    log.info('Parametros: a => "{}" [type: {}] b=> "{}" [type: {}]'.format(a, type(a), b, type(b)))
     if type(a) != str or type(b) != str:
         raise ValueError('Tipos de entrada invalidos, esta función solo acepta dos strings como entrada.')
-    print(a.encode('utf-8'))
-    print(b.encode('utf-8'))
     la = len(a)
     lb = len(b)
-    logging.info('Largo de a => {} largo de b => {}'.format(la, lb))
+    log.info('Largo de a => {} largo de b => {}'.format(la, lb))
     if la > lb:
-        logging.info('"A" es la cadena más larga ({} caracteres)'.format(la))
+        log.info('"A" es la cadena más larga ({} caracteres)'.format(la))
         return a
     elif lb > la:
-        logging.info('"B" es la cadena más larga ({} caracteres)'.format(lb))
+        log.info('"B" es la cadena más larga ({} caracteres)'.format(lb))
         return b
     else:
-        logging.info('Ambas cadenas tienen el mismo largo')
+        log.info('Ambas cadenas tienen el mismo largo')
         raise ValueError('Ambos parametros tienen el mismo largo, no es posible determinar cual cadena es de mayor tamaño.')
 
 compareStrings('aaaaa', 'asd' * 10)
-compareStrings('aaaaa', '\xC0')
+# compareStrings('aaaaa', b'\xC0'.decode('utf-8'))
 #compareStrings(200, 30)
